cb239ba7ab1350e30a7648c0cb24e2b5d62ac912
kirill pavlov <2020kirillp@gmail.com>
2024-12-01 12:52:47 -0800

    Outlines for person asbtraction and children classes done

---
+++ Player.cpp
@@ -0,0 +1,28 @@
+#include "Player.h"
+#include "Person.h"
+#include <string>
+
+using namespace std;
+
+Player::Player() : Person()
+{
+}
+
+Player::Player(string name) : Person()
+{
+    name = name;
+}
+
+void Player::placeBet(int bet)
+{
+}
+
+int Player::payDealer(int amount)
+{
+    // dealerMoney += amount;
+}
+
+void Player::updateMoney(int money)
+{
+    // playerMoney = money;
+}
--- Player.h
+++ Player.h
@@ -8,6 +8,15 @@ class Player : Person
 {
 private:
     std::string name;
+
+public:
+    // constructors
+    Player();
+    Player(std::string name);
+
+    void placeBet(int bet);
+    int payDealer(int amount);
+    void updateMoney(int money);
 };
 
 #endif
