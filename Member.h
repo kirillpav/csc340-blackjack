b58c21d9873847045e8177e8b8211402a3621850
Cedric-Blanco <158394920+Cedric-Blanco@users.noreply.github.com>
2024-12-06 13:23:23 -0800

    Add files via upload
    Fixed Member errors in my compiler, but won't deploy other methods and commented them out for now until updating with the current variables we have from other files.

--- Member.h
+++ Member.h
@@ -1,20 +1,28 @@
-class Member {
-    private: 
+#include "Media.h"
+#include <vector>
+#ifndef MEMBER_H
+#define MEMBER_H
+class Member
+{
+private:
     std::string userName;
     int memberId;
-    std::vector<Media> borrowedMedia; // push_back to add, remove or pop to return an item. 
-    
-    public: 
+    std::vector<Media> borrowedMedia; // push_back to add, remove or pop to return an item.
+
+public:
+    Member(const std::string& userName, int memberId) : userName(userName), memberId(memberId) {}
     Member();
-    
+
     Member(std::string userName, int memberId);
-    
+
     std::string getUserName() const;
-    void setUserName(std::string newName);
-    
-    int getMemberId(int newMemberId) const;
-    void setMemberId();
-    
-    void addMedia(const Media& mediaItem);
-    void returnMedia(const std::string& mediaItemName);
-}
+    void setUserName(const std::string& newName);
+
+    int getMemberId() const;
+    void setMemberId(int newMemberId);
+
+    void addMedia(const Media &mediaItem);
+    void returnMedia(const std::string &mediaItemName);
+    void printMedia() const;
+};
+#endif
